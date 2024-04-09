8cb419d1f927b4eec0ed83c113ba5b397317516b
i8g9 <imazaryag@gmail.com>
2024-04-09 21:03:30 +0700

    [ADD] Added principles page

---
+++ src/app/principles/page.tsx
@@ -0,0 +1,51 @@
+export default function Principles() {
+    return (
+        // <PrinciplesComponent/>
+        <div className="bg-base-200">
+            <h1 className="text-center text-5xl font-bold pt-8">Our Principles</h1>
+            <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mb-16 gap-4 auto-rows-auto sm:mx-8 lg:mx-16">
+                <div className="card bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">Accessibility</h2>
+                        <p>The Begin Token aims to make blockchain education and cryptocurrency knowledge accessible to people of all backgrounds and technical expertise levels.</p>
+                    </div>
+                </div>
+
+                <div className="card bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">Transparency</h2>
+                        <p>The project is committed to operating with full transparency, providing clear information about the token's mechanics, usage, and development roadmap.</p>
+                    </div>
+                </div>
+
+                <div className="card bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">Community-Driven</h2>
+                        <p>The Begin Token ecosystem will be shaped by the input and participation of its community members, fostering an inclusive and collaborative environment.</p>
+                    </div>
+                </div>
+
+                <div className="card bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">Innovation</h2>
+                        <p>The project embraces the innovative potential of blockchain technology and cryptocurrencies, seeking to showcase their transformative applications.</p>
+                    </div>
+                </div>
+
+                <div className="card bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">Integrity</h2>
+                        <p>The Begin Token team is dedicated to upholding the highest standards of integrity and ethical conduct in the development and promotion of the project.</p>
+                    </div>
+                </div>
+
+                <div className="card bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">Financial Empowerment</h2>
+                        <p>By educating users about cryptocurrency and blockchain, the Begin Token project empowers individuals to take control of their financial futures.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
