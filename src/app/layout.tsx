eeefad256659cd5764c3871fa2803ad9a31cdbee
i8g9 <imazaryag@gmail.com>
2024-04-09 21:16:16 +0700

    [EDIT] Modified layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,55 +20,56 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-      {/* <Header/> */}
-      <header className="text-white py-4 sm:px-4 lg:px-12 bg-base-200">
-        <nav className="flex items-center justify-between">
-          <div className="flex items-center">
-            <Link href="/">
-              <Image src={logo} alt="Logo" width={60} height={60} className='rounded-full' />
-            </Link>
-          </div>
-          <div className="flex items-center">
-            <details className="dropdown dropdown-end">
-              <summary className="m-2 btn">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={2}
-                  stroke="currentColor"
-                  className="w-8 h-8"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg>
-              </summary>
-              <ul className="p-4 shadow menu dropdown-content z-[1] bg-black dark:bg-base-400 rounded-box text-lg">
-                <li>
-                  <Link href="/faq" className="hover:text-gray-300">
-                    FAQ
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/principles" className="hover:text-gray-300">
-                    Principles
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/roadmap" className="hover:text-gray-300">
-                    Roadmap
-                  </Link>
-                </li>
-              </ul>
-            </details>
-          </div>
-        </nav>
-      </header>
-
+      <body className={inter.className}>
+        {/* <Header/> */}
+        <header className="text-white py-4 sm:px-4 lg:px-12 bg-base-200">
+          <nav className="flex items-center justify-between">
+            <div className="flex items-center">
+              <Link href="/">
+                <Image src={logo} alt="Logo" width={60} height={60} className='rounded-full' />
+              </Link>
+            </div>
+            <div className="flex items-center">
+              <details className="dropdown dropdown-end">
+                <summary className="m-2 btn">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="w-8 h-8"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    />
+                  </svg>
+                </summary>
+                <ul className="p-4 shadow menu dropdown-content z-[1] bg-black dark:bg-base-400 rounded-box text-lg">
+                  <li>
+                    <Link href="/faq" className="hover:text-gray-300">
+                      FAQ
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/principles" className="hover:text-gray-300">
+                      Principles
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/roadmap" className="hover:text-gray-300">
+                      Roadmap
+                    </Link>
+                  </li>
+                </ul>
+              </details>
+            </div>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,21 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="">
-        {/* <Landing/> */}
-        <div className="hero bg-base-200">
-          <div className="hero-content text-center pt-24 pb-24 lg:h-screen ">
-            <div className="max-w-md">
-              <h1 className="text-5xl font-bold">Introducing Begin Token</h1>
-              <p className="py-6 text-left">Our goal is to provide free, accessible education to users, with the aim of driving widespread adoption of blockchain technology.</p>
-              <button className="btn bg-green-200 text-black hover:bg-green-400 hover:text-black">
-                <Link href="/faq">
-                  Learn More
-                </Link>
-              </button>
-            </div>
+    <div className="">
+      {/* <Landing/> */}
+      <div className="hero bg-base-200">
+        <div className="hero-content text-center pt-24 pb-24 lg:h-screen ">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Introducing Begin Token</h1>
+            <p className="py-6 text-left">Our goal is to provide free, accessible education to users, with the aim of driving widespread adoption of blockchain technology.</p>
+            <button className="btn bg-green-200 text-black hover:bg-green-400 hover:text-black">
+              <Link href="/faq">
+                Learn More
+              </Link>
+            </button>
           </div>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
