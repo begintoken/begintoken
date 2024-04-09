2a71d8eccd0cf528a2bdf2495807dd63b56f4b7b
i8g9 <imazaryag@gmail.com>
2024-04-09 21:04:00 +0700

    [ADD] Added roadmap page

---
+++ src/app/roadmap/page.tsx
@@ -0,0 +1,94 @@
+export default function Roadmap() {
+    return (
+        // <RoadmapComponent/>
+        <div className="bg-base-200">
+            <div className="flex justify-center py-4">
+                <h1 className="text-5xl font-bold">Our Roadmap</h1>
+            </div>
+            <div className="flex justify-center py-8">
+                <ol className="border-s border-gray-200 dark:border-gray-700">
+                    <li className="mb-10 ms-6">
+                        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                            Phase 1: Market Research
+                        </h3>
+                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                            Conducted comprehensive market research to identify opportunities and challenges.
+                        </p>
+                    </li>
+                    <li className="mb-10 ms-6">
+                        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                            Phase 2: Development and Testing
+                        </h3>
+                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                            Developed and thoroughly test the Begin Token.
+                        </p>
+                    </li>
+                    <li className="mb-10 ms-6">
+                        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                            Phase 3: Deployment to Solana Mainnet
+                        </h3>
+                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                            Deployed the project to the Solana mainnet.
+                        </p>
+                    </li>
+                    <li className="mb-10 ms-6">
+                        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                            Phase 4: Website Deployment
+                        </h3>
+                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                            Launched the project's website to provide information and access to the platform.
+                        </p>
+                    </li>
+                    <li className="mb-10 ms-6 border border-green-500 rounded-lg bg-green-500 p-4">
+                        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                            Phase 5: Presale
+                        </h3>
+                        <p className="mb-4 text-base font-normal text-black dark:text-white">
+                            Conduct a presale event to raise funds and generate initial interest.
+                        </p>
+                    </li>
+                    <li className="mb-10 ms-6">
+                        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                            Phase 6: ICO
+                        </h3>
+                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                            Launch the initial coin offering (ICO) to further expand the project's reach.
+                        </p>
+                    </li>
+                    <li className="mb-10 ms-6">
+                        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                            Phase 7: Listing on Coinbase
+                        </h3>
+                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                            List the project's token on the Coinbase exchange to improve visibility and accessibility.
+                        </p>
+                    </li>
+                    <li className="mb-10 ms-6">
+                        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                            Phase 8: DEX Pool Listing
+                        </h3>
+                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                            Integrate the project's token with decentralized exchanges (DEXs) to provide additional liquidity.
+                        </p>
+                    </li>
+                    <li className="mb-10 ms-6">
+                        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                            Phase 9: Ongoing Development
+                        </h3>
+                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                            Continuously improve and expand the project's features and functionality based on user feedback.
+                        </p>
+                    </li>
+                    <li className="mb-10 ms-6">
+                        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                            Phase 10: Wallet Creation
+                        </h3>
+                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                            Develop a dedicated wallet application to facilitate secure storage and management of the project's token.
+                        </p>
+                    </li>
+                </ol>
+            </div>
+        </div>
+    );
+}
