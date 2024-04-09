e3b058bf6cdbb605fb4482ee051df33e553f8848
i8g9 <imazaryag@gmail.com>
2024-04-09 22:10:44 +0700

    [EDIT] Edited landing page hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="hero-content text-center pt-24 pb-24 lg:h-screen ">
           <div className="max-w-md">
             <h1 className="text-5xl font-bold">Introducing Begin Token</h1>
-            <p className="py-6 text-left">Our goal is to provide free, accessible education to users, with the aim of driving widespread adoption of blockchain technology.</p>
+            <p className="py-6 text-left">Our goal is to provide free and accessible education to everyone, with the aim of driving widespread adoption of blockchain technology.</p>
             <button className="btn bg-green-200 text-black hover:bg-green-400 hover:text-black">
               <Link href="/faq">
                 Learn More
