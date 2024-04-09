0e1c39ede13500e71821b35db87404317886fddd
i8g9 <imazaryag@gmail.com>
2024-04-09 21:01:39 +0700

    [ADD] Added faq page

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,80 @@
+export default function Faq() {
+    return (
+        // <FaqComponent/>
+        <div>
+            <section className="bg-base-200 dark:bg-gray-900">
+                <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 sm:mx-8 lg:mx-32">
+                    <h2 className="mb-8 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Frequently asked questions</h2>
+                    <div className="pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2">
+                        <div>
+                            <div className="mb-10">
+                                <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                    <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                                    What is the Begin Token ($BEGIN)?
+                                </h3>
+                                <p className="text-gray-500 dark:text-gray-400">The Begin Token is a crypto made to teach new people about blockchain and digital coins.</p>
+                            </div>
+                            <div className="mb-10">
+                                <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                    <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                                    Why was the Begin Token created?
+                                </h3>
+                                <p className="text-gray-500 dark:text-gray-400">The goal is to help explain crypto and blockchain in an easy way. There's not enough simple info out there right now.</p>
+                            </div>
+                            <div className="mb-10">
+                                <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                    <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                                    How can I get Begin Tokens?
+                                </h3>
+                                <p className="text-gray-500 dark:text-gray-400">You can buy Begin Tokens during the public sale events like the initial coin offering (ICO), Central Exchanges (CEX) like Coinbase and Decentralized Exchanges (DEX) like Raydium</p>
+                            </div>
+                            <div className="mb-10">
+                                <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                    <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                                    What are the key features of the Begin Token?
+                                </h3>
+                                <p className="text-gray-500 dark:text-gray-400">It has a fixed total supply of 50 million tokens, optional token burning, and no required fees.</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="mb-10">
+                                <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                    <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                                    How can I use my Begin Tokens?
+                                </h3>
+                                <p className="text-gray-500 dark:text-gray-400">You can use your Begin Tokens to learn how to safely acquire and manage cryptocurrencies. The educational platform is accessible at no cost to all users.</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="mb-10">
+                                <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                    <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                                    Is the Begin Token a good investment?
+                                </h3>
+                                <p className="text-gray-500 dark:text-gray-400"> While the Begin Token is primarily focused on education rather than investment, its fixed total supply means the token may gain value over time as adoption grows. However, the project's main goal is to teach people about blockchain technology, not to be a speculative investment.</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="mb-10">
+                                <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                    <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                                    What kind of learning content will be available?
+                                </h3>
+                                <p className="text-gray-500 dark:text-gray-400">The platform will have articles, tutorials, videos, and a glossary to teach about crypto.</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="mb-10">
+                                <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                    <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                                    How can I stay updated on the project?
+                                </h3>
+                                <p className="text-gray-500 dark:text-gray-400">Follow the Begin Token's official social media accounts and check the website for the latest news.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
